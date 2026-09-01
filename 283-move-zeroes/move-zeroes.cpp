2e53bdcc33b431328b103fdbf2390d06fbf1c7aa
Adrish Purkayastha <purkayasthashan@gmail.com>
2026-09-02 00:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (98.79%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[index] = nums[i];
+                index += 1;
+            }
+        }
+        for (int j=index;j<n;j++){
+            nums[j]=0;
+        }
+
+
+        
+    }
+};
